d541c1aef64c5c6b12cd1ded74fea88e26ffea02
John Obi <johngorithm@gmail.com>
2018-12-05 09:11:21 +0100

    feat(connect-view): connect recycler view layout file to main activity
    - instantiate layout manager for the list view
    - referrence recycler view from the main activity
    
    [Delivers #155768857]

--- app/src/main/java/com/jxw/git_hub_users/MainActivity.java
+++ app/src/main/java/com/jxw/git_hub_users/MainActivity.java
@@ -2,12 +2,29 @@ package com.jxw.git_hub_users;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private RecyclerView mRecyclerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mRecyclerView = findViewById(R.id.gh_users_recycler_view);
+
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        RecyclerView.LayoutManager mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // Adapter will be instantiated and set here
+        mRecyclerView.setAdapter(null);
+
     }
 }
