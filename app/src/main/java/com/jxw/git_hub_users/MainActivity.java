047ee45620711cc8309c1f78a715bde5a8e5c051
John Obi <johngorithm@gmail.com>
2018-12-06 15:12:06 +0100

    feat(ui-interactivity): lauch detail activity from main activity
    - add DetailActivity activity class
    - add click event on user card view
    - attach a click event listner to the card view
    
    [Delivers #155820624]

--- app/src/main/java/com/jxw/git_hub_users/MainActivity.java
+++ app/src/main/java/com/jxw/git_hub_users/MainActivity.java
@@ -1,19 +1,23 @@
 package com.jxw.git_hub_users;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
 
 public class MainActivity extends AppCompatActivity {
 
     private RecyclerView mRecyclerView;
+    CardView userCard;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         mRecyclerView = findViewById(R.id.gh_users_recycler_view);
 
 
@@ -26,5 +30,18 @@ public class MainActivity extends AppCompatActivity {
         // Adapter will be instantiated and set here
         mRecyclerView.setAdapter(null);
 
+        addCardClickListner();
     }
+
+    private void addCardClickListner() {
+        userCard = findViewById(R.id.user_card);
+        userCard.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent details = new Intent(MainActivity.this, DetailViewActivity.class);
+                startActivity(details);
+            }
+        });
+    }
+
 }
